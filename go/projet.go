e1efd866d9d46f0d373b7b02b47372bbbc139b2b
Tharathathal <thaliacharton@free.fr>
2023-12-21 10:40:47 +0100

    func min

--- go/projet.go
+++ go/projet.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func min(a,b,c  int)int{
+	if a<=b && a<=c {
+		return a
+	}
+	elif b<=a && b<=c {
+		return b
+	}
+	return c
+}
+
 func main() {
 	fmt.Println("Hello")
 }
