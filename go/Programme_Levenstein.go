08ac344551c03346bd50d5cb349d3985a5c9da60
Tharathathal <thaliacharton@free.fr>
2024-01-08 15:32:11 +0100

    ok

---
+++ go/Programme_Levenstein.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/arbovm/levenshtein"
+)
+
+func main() {
+	mots := []string{
+		"chat", "chien", "maison", "voiture", "arbre",
+		"ordinateur", "fenêtre", "porte", "jardin", "fleur",
+		"soleil", "lune", "étoile", "montagne", "plage",
+		"mer", "rivière", "oiseau", "poisson", "pomme",
+		"banane", "orange", "kiwi", "fraise", "raisin",
+		"tomate", "salade", "pain", "fromage", "café",
+		"thé", "vélo", "train", "avion", "bus",
+		"école", "professeur", "élève", "livre", "crayon",
+		"piano", "guitare", "musique", "film", "photo",
+		"amour", "amitié", "rire", "bonheur",
+	}
+
+	s1 := "kitten"
+	s2 := "sitting"
+	fmt.Printf("The distance between %v and %v is %v\n",
+		s1, s2, levenshtein.Distance(s1, s2))
+	// -> The distance between kitten and sitting is 3
+}
