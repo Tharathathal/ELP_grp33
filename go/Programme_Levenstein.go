445455efc49c8bdb2caa6941b1b8cc1d82243729
Tharathathal <thaliacharton@free.fr>
2024-01-08 16:54:46 +0100

    :)

--- go/Programme_Levenstein.go
+++ go/Programme_Levenstein.go
@@ -18,16 +18,26 @@ func main() {
 	fmt.Scanf("%s", &mot_client)
 
 	var mots = []string{
-		"chat", "chien", "maison", "voiture", "arbre",
-		"ordinateur", "fenêtre", "porte", "jardin", "fleur",
-		"soleil", "lune", "étoile", "montagne", "plage",
-		"mer", "rivière", "oiseau", "poisson", "pomme",
-		"banane", "orange", "kiwi", "fraise", "raisin",
-		"tomate", "salade", "pain", "fromage", "café",
-		"thé", "vélo", "train", "avion", "bus",
-		"école", "professeur", "élève", "livre", "crayon",
-		"piano", "guitare", "musique", "film", "photo",
-		"amour", "amitié", "rire", "bonheur",
+		"lecture", "cinéma", "voyage", "photographie", "art",
+		"musique", "danse", "sport", "cuisine", "jardinage",
+		"astronomie", "science", "technologie", "informatique", "programmation",
+		"histoire", "philosophie", "psychologie", "sociologie", "langues",
+		"écriture", "poésie", "théâtre", "jeux de société", "jeux vidéo",
+		"nature", "animaux", "environnement", "randonnée", "camping",
+		"yoga", "méditation", "spiritualité", "mode", "design",
+		"bricolage", "cuisine", "oenologie", "voyage spatial", "archéologie",
+		"vélo", "voitures", "aventure", "aviation", "plongée",
+		"philanthropie", "volontariat", "éducation", "innovation", "entrepreneuriat",
+		"amour", "joie", "tristesse", "colère", "peur",
+		"surprise", "enthousiasme", "calme", "espoir", "doute",
+		"déception", "excitation", "stress", "réconfort", "courage",
+		"fierté", "humilité", "gaieté", "mélancolie", "émotion",
+		"sentiment", "affection", "intimité", "complicité", "attirance",
+		"répulsion", "frustration", "plaisir", "divertissement", "créativité",
+		"imagination", "exploration", "découverte", "apprentissage", "expression",
+		"communication", "connexion", "introspection", "interactions", "collaboration",
+		"inclusion", "autonomie", "accomplissement", "épanouissement", "bienveillance",
+		"altruisme", "solidarité", "gratitude", "tolérance", "acceptation",
 	}
 	for i := 0; i < len(mots); i++ {
 		lev_dist(mot_client, mots[i])
