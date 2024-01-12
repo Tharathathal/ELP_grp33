85fd3dd6cdf283ec6e4b3f6f5a55ecb01e96fae8
Tharathathal <thaliacharton@free.fr>
2024-01-12 14:46:16 +0100

    add TCP server

--- go/Programme_Levenstein.go
+++ go/Programme_Levenstein.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"time"
 
 	"github.com/arbovm/levenshtein"
 )
 
 func lev_dist(mot_client, mot string) {
-	fmt.Printf("The distance between %v and %v is %v\n",
-		mot_client, mot, levenshtein.Distance(mot_client, mot))
-
+	//Calcule la distance de Levenshtein grâce à la library
+	fmt.Printf("- %v est de %v\n",
+		mot, levenshtein.Distance(mot_client, mot))
 }
 
-func main() {
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Connexion établie.")
+
+	//Communication avec le serveur
+	conn.Write([]byte("Quel prénom voulez-vous comparer ?"))
+
+	buffer := make([]byte, 1024)
+	mot_client, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Erreur de lecture:", err)
+		return
+	}
+
+	//Traitement des données du client et retour des informations
 	var prénoms = []string{
 		"Alexandre", "Thomas", "Antoine", "Nicolas", "Pierre",
 		"Paul", "François", "Louis", "Charles", "Jean",
@@ -38,8 +53,38 @@ func main() {
 		"Léonie",
 	}
 
-	for i := 0; i < len(mots); i++ {
+	fmt.Println("La distance de Levenshtein entre %v et :\n", mot_client)
+
+	//Comparaison à chaque prénom de la liste avec des goroutines
+	for i := 0; i < len(prénoms); i++ {
 		go lev_dist(mot_client, prénoms[i])
 		time.Sleep(10 * time.Millisecond)
+	}
+
+	//Incrémente la liste des prénoms
+	prénoms = append(prénoms, mot_client)
+}
+
+func main() {
+	//Attente de connection TCP
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Serveur en attente de connexion...")
+
+	for {
+		//Accepte connexion entrante
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		//Chaque client géré dans une goroutine
+		go handleClient(conn)
 	}
 }
