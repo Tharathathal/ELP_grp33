a811fd2cec4c1a4f9c473f4fff4da3b4c369244b
Tharathathal <thaliacharton@free.fr>
2024-01-12 14:24:25 +0100

    create file

--- go/Programme_Levenstein.go
+++ go/Programme_Levenstein.go
@@ -14,33 +14,6 @@ func lev_dist(mot_client, mot string) {
 }
 
 func main() {
-	fmt.Print("Choisissez le mot qui servira de comparatif avec la liste: ")
-	var mot_client string
-	fmt.Scanf("%s", &mot_client)
-
-	var mots = []string{
-		"lecture", "cinéma", "voyage", "photographie", "art",
-		"musique", "danse", "sport", "cuisine", "jardinage",
-		"astronomie", "science", "technologie", "informatique", "programmation",
-		"histoire", "philosophie", "psychologie", "sociologie", "langues",
-		"écriture", "poésie", "théâtre", "jeux de société", "jeux vidéo",
-		"nature", "animaux", "environnement", "randonnée", "camping",
-		"yoga", "méditation", "spiritualité", "mode", "design",
-		"bricolage", "cuisine", "oenologie", "voyage spatial", "archéologie",
-		"vélo", "voitures", "aventure", "aviation", "plongée",
-		"philanthropie", "volontariat", "éducation", "innovation", "entrepreneuriat",
-		"amour", "joie", "tristesse", "colère", "peur",
-		"surprise", "enthousiasme", "calme", "espoir", "doute",
-		"déception", "excitation", "stress", "réconfort", "courage",
-		"fierté", "humilité", "gaieté", "mélancolie", "émotion",
-		"sentiment", "affection", "intimité", "complicité", "attirance",
-		"répulsion", "frustration", "plaisir", "divertissement", "créativité",
-		"imagination", "exploration", "découverte", "apprentissage", "expression",
-		"communication", "connexion", "introspection", "interactions", "collaboration",
-		"inclusion", "autonomie", "accomplissement", "épanouissement", "bienveillance",
-		"altruisme", "solidarité", "gratitude", "tolérance", "acceptation",
-		"alcoolisme",
-	}
 	var prénoms = []string{
 		"Alexandre", "Thomas", "Antoine", "Nicolas", "Pierre",
 		"Paul", "François", "Louis", "Charles", "Jean",
@@ -64,6 +37,7 @@ func main() {
 		"Francesca", "Elena", "Luisa", "Gina", "Rita",
 		"Léonie",
 	}
+
 	for i := 0; i < len(mots); i++ {
 		go lev_dist(mot_client, prénoms[i])
 		time.Sleep(10 * time.Millisecond)
