d6712c0581e728d405b600465d8fb77f9d9027b6
Teo971 <frateo971@hotmail.com>
2024-01-12 15:15:40 +0100

    ok

---
+++ go/Client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	// connect to server
+	conn, _ := net.Dial("tcp", "127.0.0.1:8000")
+	for {
+		// what to send?
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Donnez-nous votre prénom pour tester votre compatibilité: ")
+		text, _ := reader.ReadString('\n')
+		fmt.Printf("Votre prénom est: ", text)
+		// send to server
+		fmt.Fprintf(conn, text+"\n")
+		// wait for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
