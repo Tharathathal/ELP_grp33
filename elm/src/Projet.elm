434eee72a2229b1fdc911268cea3b5ad641626d0
Tharathathal <thaliacharton@free.fr>
2024-01-22 14:56:45 +0100

    sandbox to element

--- elm/src/Projet.elm
+++ elm/src/Projet.elm
@@ -5,13 +5,14 @@ module Projet exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Http
 
 
 
 -- MAIN
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, subscriptions = subscriptions, update = update, view = view }
 
 
 
@@ -19,9 +20,12 @@ main =
 
 type alias Model = String
 
-init : Model
-init =
-  " "
+init : () -> (Model, Cmd msg)
+init _ =
+  ("init", _)
+
+
+-- SUBSCRIPTIONS
 
 
 
@@ -30,11 +34,11 @@ init =
 type Msg
   = NewWord
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd msg)
 update msg model =
   case msg of
     NewWord ->
-        mot
+      ("mot", _)
 
 
 
