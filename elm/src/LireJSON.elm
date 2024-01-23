5e758316c917ddf1b5fa881f6bc090beec96f6e0
Teo971 <frateo971@hotmail.com>
2024-01-23 14:34:12 +0100

    Lecture BON !!!!!!

--- elm/src/LireJSON.elm
+++ elm/src/LireJSON.elm
@@ -1,40 +1,30 @@
 module LireJSON exposing (..)
 
 import Browser
-import Http
 import Html exposing (Html, div, text)
+import Http
+import Json.Decode exposing (Decoder, field, list, map, map2, map3, map4, nullable, string)
 import Result exposing (Result)
-import Json.Decode exposing (..)
 
--- Main --
-main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
+
+-- MODEL
+type alias Word =
+    { word : String
+    , phonetics : List Phonetic
+    , meanings : List Meaning
     }
 
--- SUBSCRIPTIONS --
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
---- MODEL --- 
-type Model
-    = Loading
-    | Loaded Word
-    | Failed
-
 type alias Phonetic =
-    { text : String
-    , audio : Maybe String
+    { audio : Maybe String
+    }
+
+type alias License = 
+    { name : Maybe String
+    , url : Maybe String
     }
 
 type alias Definition =
-    { definition : String
-    , example : String
+    { definitions : String
     , synonyms : List String
     , antonyms : List String
     }
@@ -44,86 +34,166 @@ type alias Meaning =
     , definitions : List Definition
     }
 
-type alias Word =
-    { word : String
-    , phonetic : String
-    , phonetics : List Phonetic
-    , origin : String
-    , meanings : List Meaning
-    }
+type Model
+    = Failure Http.Error
+    | Loading
+    | Success (List Word)
 
+-- MESSAGES
+type Msg
+    = DataReceived (Result Http.Error (List Word))
+
+-- INIT
 init : () -> (Model, Cmd Msg)
 init _ =
-    (Loading, fetchWord)
-
-decodePhonetic : Decoder Phonetic
-decodePhonetic =
-    map2 Phonetic
-        (field "text" string)
-        (field "audio" (nullable string))
-
-decodeDefinition : Decoder Definition
-decodeDefinition =
-    map4 Definition
-        (field "definition" string)
-        (field "example" string)
-        (field "synonyms" (list string))
-        (field "antonyms" (list string))
-
-decodeMeaning : Decoder Meaning
-decodeMeaning =
-    map2 Meaning
-        (field "partOfSpeech" string)
-        (field "definitions" (list decodeDefinition))
-
-decodeWord : Decoder Word
-decodeWord =
-    map5 Word
-        (field "word" string)
-        (field "phonetic" string)
-        (field "phonetics" (list decodePhonetic))
-        (field "origin" string)
-        (field "meanings" (list decodeMeaning))
-
-type Msg
-    = FetchWord
-    | ReceiveWord (Result Http.Error Word)
-
---- LIEN QUI RECUPERE LE JSON ---
-fetchWord : Cmd Msg
-fetchWord = 
-    Http.get
+    (Loading
+    , Http.get
         { url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-        , expect = Http.expectJson ReceiveWord decodeWord
+        , expect = Http.expectJson DataReceived decoder
         }
+    )
 
+-- UPDATE
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        FetchWord ->
-            (Loading,fetchWord)
-        ReceiveWord result ->
-          case result of 
-            Ok word ->
-                (Loaded word,Cmd.none)
-            Err _ ->
-                (Failed, Cmd.none)
+        DataReceived result ->
+            case result of
+                Ok newData ->
+                    (Success newData, Cmd.none)
+                Err err ->
+                    (Failure err, Cmd.none)
 
+errorToString : Http.Error -> String
+errorToString error =
+    case error of
+        Http.BadUrl url ->
+            "The URL " ++ url ++ " was invalid"
+        Http.Timeout ->
+            "Unable to reach the server, try again"
+        Http.NetworkError ->
+            "Unable to reach the server, check your network connection"
+        Http.BadStatus 500 ->
+            "The server had a problem, try again later"
+        Http.BadStatus 400 ->
+            "Verify your information and try again"
+        Http.BadStatus x ->
+            "Unknown error with status " ++ String.fromInt x
+        Http.BadBody errorMessage ->
+            errorMessage
 
+-- VIEW
 view : Model -> Html Msg
 view model =
-  case model of
-    Loading ->
-      div [] [ text "Chargement..." ]
-    Loaded word ->
-      -- Utilisez les données word pour afficher ce que vous voulez
-      div [] [ text "Données chargées:", text (word.word) ]
-    Failed ->
-      div [] [ text "Échec du chargement des données" ]
+    case model of
+        Failure error ->
+            text ("Error: " ++ errorToString error)
+
+        Loading ->
+            text "Loading..."
+
+        Success newData ->
+            div []
+                [ div [] [ text "Words: ", viewWords newData ]
+                ]
+
+viewWords : List Word -> Html Msg
+viewWords words =
+    case words of
+        [] ->
+            div [] [ text "No data available." ]
+        
+        (word :: _) ->
+            div []
+                [ div [] [ text ("Word: " ++ word.word) ]
+                , div [] [ text "Phonetics: ", viewPhonetics word.phonetics ]
+                , div [] [ text "Meanings: ", viewMeanings word.meanings ]
+                ]
+
+viewPhonetics : List Phonetic -> Html Msg
+viewPhonetics phonetics =
+    div []
+        (List.map
+            (\ph ->
+                div []
+                    [ text ("Audio: " ++ Maybe.withDefault "N/A" ph.audio)
+                    ]
+            )
+            phonetics
+        )
 
 
+viewMeanings : List Meaning -> Html Msg
+viewMeanings meanings =
+    div []
+        (List.map
+            (\m ->
+                div []
+                    [ text ("Part of Speech: " ++ m.partOfSpeech)
+                    , div [] [ text "Definitions: ", viewDefinitions m.definitions ]
+                    ]
+            )
+            meanings
+        )
 
+viewDefinitions : List Definition -> Html Msg
+viewDefinitions definitions =
+    div []
+        (List.map
+            (\d ->
+                div []
+                    [ text ("Definition: " ++  d.definitions)
+                    , div [] [ text ("Synonyms: " ++ String.join ", " d.synonyms) ]
+                    , div [] [ text ("Antonyms: " ++ String.join ", " d.antonyms) ]
+                    ]
+            )
+            definitions
+        )
 
+-- DECODER
+decoder : Decoder (List Word)
+decoder =
+    list decoderWord
 
+decoderWord : Decoder Word
+decoderWord =
+    map3 Word
+        (field "word" string)
+        (field "phonetics" (list decoderPhonetic))
+        (field "meanings" (list decoderMeaning))
+        
 
+decoderPhonetic : Decoder Phonetic
+decoderPhonetic =
+    map Phonetic
+        (field "audio" (nullable string))
 
+decoderLicense : Decoder License
+decoderLicense = 
+    map2 License
+        (field "name" (nullable string))
+        (field "url"(nullable string))        
+
+decoderDefinition : Decoder Definition
+decoderDefinition =
+    map3 Definition
+        (field "definition" string)
+        (field "synonyms" (list string))
+        (field "antonyms" (list string))
+        
+
+decoderMeaning : Decoder Meaning
+decoderMeaning =
+    map2 Meaning
+        (field "partOfSpeech" string)
+        (field "definitions" (list decoderDefinition))
+
+-- MAIN
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
